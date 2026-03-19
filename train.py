f8957d7190fd9080856f9d6671930c99f051a788
edwarbudiman <business.edwarb@gmail.com>
2026-03-19 12:10:19 +1100

    canvas repo

--- train.py
+++ train.py
@@ -45,9 +45,9 @@ wandb_project = 'owt'
 wandb_run_name = 'gpt2' # 'run' + str(time.time())
 # data
 dataset = 'openwebtext'
-gradient_accumulation_steps = 5 * 8 # used to simulate larger batch sizes
-batch_size = 12 # if gradient_accumulation_steps > 1, this is the micro-batch size
-block_size = 1024
+gradient_accumulation_steps = 1
+batch_size = 64
+block_size = 256 # context of up to 256 previous characters
 # model
 n_layer = 12
 n_head = 12
