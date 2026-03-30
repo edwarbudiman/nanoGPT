c630d5dcf50f9d22cc7776b282683007a7f21e33
edwarbudiman <business.edwarb@gmail.com>
2026-03-31 06:47:35 +1100

    always save

--- config/plan-a/a_combineddefault.py
+++ config/plan-a/a_combineddefault.py
@@ -5,7 +5,7 @@ out_dir = 'out-plan-a/a-default'
 eval_interval = 500
 eval_iters = 100
 log_interval = 50
-always_save_checkpoint = True
+always_save_checkpoint = False
 
 wandb_log = False
 wandb_project = 'rocstories-plan-a'
--- config/plan-a/a_combinedheavy.py
+++ config/plan-a/a_combinedheavy.py
@@ -34,7 +34,7 @@ out_dir = 'out-plan-a/a-combined'
 eval_interval = 250
 eval_iters = 100
 log_interval = 25
-always_save_checkpoint = True
+always_save_checkpoint = False
 
 wandb_log = False
 wandb_project = 'rocstories-plan-a'
